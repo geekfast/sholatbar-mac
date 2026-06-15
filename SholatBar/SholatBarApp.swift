254ccb4e5daa66eeea7f97f47a9bd360c836afb7
Riswan Rais <riswan.rais@dana.id>
2026-06-15 14:45:18 +0700

    Initial Commit

---
+++ SholatBar/SholatBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SholatBarApp.swift
+//  SholatBar
+//
+//  Created by Riswan Rais on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SholatBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
